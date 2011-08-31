9de71a5cdd653aa1fdb19b80020526b4a5200ee2
Joshua Krall <joshuakrall@pobox.com>
2011-08-31 13:47:00 -0500

    Working on simplifying the flow even further

--- demo.rb
+++ demo.rb
@@ -4,21 +4,11 @@ Samurai.options = YAML.load(open('https://raw.github.com/FeeFighters/samurai-sin
 layout { open('https://raw.github.com/FeeFighters/samurai-sinatra-demo/master/views/layout.erb').read }
 
 get '/' do
-  payment_method = OpenStruct.new :first_name=>'Joe', :last_name=>'FeeFighter', :card_number=>'4111111111111111', :cvv=>'123'
-  erb Samurai::PaymentMethod.form_html, :locals=>{ :redirect_url=>url('/new-transaction'), :sandbox=>true, :payment_method=>payment_method }
-end
-
-get '/new-transaction' do
-  erb Samurai::Transaction.form_html, :locals=>{
-    :payment_method => Samurai::PaymentMethod.find(params[:payment_method_token]),
-    :processor_token => Samurai::Processor.the_processor.id,
-    :post_url => '/create-transaction',
-    :transaction => OpenStruct.new(:currency_code=>'US'),
-  }
+  @payment_method = OpenStruct.new :first_name=>'Joe', :last_name=>'FeeFighter', :card_number=>'4111111111111111', :cvv=>'123'
+  erb Samurai::Rails::Views.payment_method_form_html, :locals=>{ :sandbox=>true, :redirect_url=>'/create-transaction' }
 end
 
 post '/create-transaction' do
-  processor = Samurai::Processor.the_processor
-  purchase = processor.purchase params[:payment_method_token], params[:amount], params[:transaction]
-  erb Samurai::Transaction.show_html, :locals=>{ :transaction=>purchase }
+  @transaction = Samurai::Processor.the_processor.purchase params[:payment_method_token], 111.11, :descriptor=>'Katana Sword'
+  erb Samurai::Rails::Views.transaction_html
 end

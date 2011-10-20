0d5ba525b6d09e01aed943d1f16f013e190da7c7
Tisho Georgiev <tihomir.georgiev@gmail.com>
2011-10-20 15:45:27 +0300

    Kept ruby-1.9.2-p180 restriction, because of YAML serialization problems. Fixed paths to samurai.js in layout, javascript code is now the same as on the samurai website, made sure the demo works.

--- demo.rb
+++ demo.rb
@@ -8,10 +8,8 @@ Samurai.options = YAML.load(open( './samurai.yml').read).symbolize_keys
 get '/' do; haml ''; end
 
 post '/create-transaction' do
-  @payment_method = Samurai::PaymentMethod.find params[:payment_method_token]
   @transaction = Samurai::Processor.the_processor.purchase params[:payment_method_token],
                                                            111.11,
-                                                           :descriptor=>'Katana Sword'
-  @errors = erb Samurai::Rails::Views.errors_html, :layout=>false
-  erb Samurai::Rails::Views.transaction_html
+                                                           :descriptor => 'Katana Sword'
+  @transaction.to_json
 end
